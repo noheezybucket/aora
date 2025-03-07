3a403981e96c70345b774264fadae0515e848bfa
noheezybucket <seydinag023@gmail.com>
2025-03-07 00:32:15 +0000

    tabs layout done

--- app/index.tsx
+++ app/index.tsx
@@ -5,10 +5,9 @@ import { Link } from 'expo-router';
 export default function Index() {
   return (
     <View className="flex-1 justify-center items-center">
-      <Text className="font-bold text-7xl">Aora !</Text>
+      <Text className="font-pextrabold text-7xl">Aora !</Text>
       <StatusBar style="auto" />
-      <Link href="/profile" className="border bg-black text-white p-3 rounded">Go to profile</Link>
-
+      <Link href="/home" className="border bg-black text-white p-3 rounded">Go to home</Link>
     </View>
   );
 }
---
+++ app/search/[query].jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const SearchQuery = () => {
+    return (
+        <View>
+            <Text>SearchQuery</Text>
+        </View>
+    )
+}
+
+export default SearchQuery
+
+const styles = StyleSheet.create({})
