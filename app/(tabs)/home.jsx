5eb812017f79406c08e2a0fa0660b4ea2d7a32fc
noheezybucket <seydinag023@gmail.com>
2025-03-17 12:32:15 +0000

    trending videos building

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -5,12 +5,13 @@ import {images} from '../../constants'
 import SearchInput from "../../components/SearchInput";
 import Trending from "../../components/Trending";
 import EmptyState from "../../components/EmptyState";
-import {getAllPosts} from "../../lib/appwrite";
+import {getAllPosts, getLatestPosts} from "../../lib/appwrite";
 import useAppwrite from "../../lib/useAppwrite";
 import VideoCard from "../../components/VideoCard";
 
 const Home = () => {
     const {data:posts, refetch} = useAppwrite(getAllPosts);
+    const {data:latestPosts} = useAppwrite(getLatestPosts);
 
     const [refreshing, setRefreshing] = useState(false)
     const onRefresh = () => {
@@ -46,7 +47,7 @@ const Home = () => {
                         <SearchInput />
                         <View className={"w-full flex-1 pt-5 pb-8"}>
                             <Text className={'text-gray-100 text-lg font-pregular mb-3'}>Latest videos</Text>
-                            <Trending posts={[{id:1}, {id:2}, {id:3}, {id:4}, {id:5}, {id:6}] ?? []}
+                            <Trending posts={latestPosts ?? []}
                             />
                         </View>
                     </View>
